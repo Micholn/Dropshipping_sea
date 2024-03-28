05edbf233e6f0313415c3d074a3981e389893c8f
Micholn <mdiagbare@gmail.com>
2024-03-28 22:28:26 +0100

    took out the environmental variable for replacement

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,7 +41,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
 }
 
-if (process)
+if (process.env.NODE_ENV !== production) {
+  require("dotenv").config()
+}
 
 const express = require("express");
 const app = express();
