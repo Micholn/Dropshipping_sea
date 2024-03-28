340e4c7bb1adcfcca76083d79c917eda7bad098d
Micholn <mdiagbare@gmail.com>
2024-03-28 22:28:39 +0100

    took out the environmental variable for replacement

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -39,7 +39,7 @@ Passport.use( express: comfig: ss: any: : )
 dependency */
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
-}
+
 
 if (process.env.NODE_ENV !== production) {
   require("dotenv").config()
