5320c44193cc741395b224eff718c0311735e412
Micholn <mdiagbare@gmail.com>
2024-03-28 22:37:22 +0100

    took out the environmental variable for replacement

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -33,7 +33,7 @@ let jwtOptions = {
 let strategy = new jwtStrategy(jwtOptions, (jwtPaylo
 
 
-Passport.use( ex
+Passport.use( 
 if (process.env.NODE_ENV !== "production")
 
 if (process.env.NODE_ENV !== production) {
