724d26fc82ae2f17f0166f7f0bb7b54878ac99a0
Micholn <mdiagbare@gmail.com>
2024-03-31 23:54:46 +0100

    reconfigured the repo commands

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ let ExtractJwt = passportJWT.ExtractJwt;
 let jwtStrategy = passportJWT.strategy;
 
 let jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWith;
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme;
   
 }
 //eslint-disable-next-line new-cap
