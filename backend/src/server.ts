794f1dce33c1813e4fe80bf4c52ebfddab8ccbc1
Micholn <mdiagbare@gmail.com>
2024-04-01 23:55:00 +0100

    added the secret authentication

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,7 @@ let strategy = new jwtStrategy(jwtOptions, (jwtPaylo
 
 
 
-const bcrypt = require("bcrypt");/*module for hashing our users
+const bcrypt = require("bcrypt");/*module for hashing our user
 const passport = require("passport") //for authentication 
 const flash = require("express-flash");//to display messages and response
 const session = require("express-session"); //to persist users across di
