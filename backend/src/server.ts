c14a83b3b9ea16a7270784a2ff1428fe0a6cee80
Micholn <mdiagbare@gmail.com>
2024-04-05 00:19:37 +0100

    found an alternative to the hashing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -43,7 +43,7 @@ const session = require("express-session");
 const users = [] //create a variable and storing our users in an empty array instead of a database 
 
 app.set("view-engine", "ejs")
-app.use(express.urlencoded({ extended: false })); /**te*
+app.use(express.urlencoded({ extended: false })); /**t*
 email and password from our forms and access them in a request variable inside the post method */
 app.use(flash());
 app.use(session({
