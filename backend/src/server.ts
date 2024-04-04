5e7d195a53d5c6ff8a96adf8dea27424a0953a6d
Micholn <mdiagbare@gmail.com>
2024-04-04 23:54:04 +0100

    found an alternative to the hashing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -47,7 +47,7 @@ app.use(express.urlencoded({ extended: false })); /**telling our app that we wan
 email and password from our forms and access them in a request variable inside the post method */
 app.use(flash());
 app.use(session({
-  secret: process.env.SESSION_SECRET, //going to encrypt all of our info
+  secret: process.env.SESSION_SECRET, //going to encrypt all of our in
   resave: false,             
   saveUninitialized: false   
 }));
