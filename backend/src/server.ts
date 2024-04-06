b8821be7991eaee79905df87ceb862fa39ce9079
Micholn <mdiagbare@gmail.com>
2024-04-06 23:04:54 +0100

    added the flash function and module

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,7 +44,7 @@ const users = [] //create a variable and storing our users in an empty array ins
 
 app.set("view-engine", "ejs")
 app.use(express.urlencoded({ extended: false })); *
-email and password from our forms and acces */
+email and password from our forms and acce */
 app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET, 
