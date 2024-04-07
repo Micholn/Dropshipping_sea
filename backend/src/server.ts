9947367904cf51127270ebb436e03dfd3ed2a799
Micholn <mdiagbare@gmail.com>
2024-04-07 23:56:59 +0100

    method override

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -43,7 +43,7 @@ const session = require("express-session");
 const users = [] //create a variable and storing our users in an empty array instead of a database 
 
 app.set("view-engine", "ejs")
-app.use(express.urlencoded({ extended: f
+app.use(express.urlencoded({ extended:
 email a
 app.use(flash());
 app.use(session({
