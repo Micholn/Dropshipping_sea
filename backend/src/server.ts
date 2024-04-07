2b08932ebf703a9af6eeba0c669d52058d4f43b6
Micholn <mdiagbare@gmail.com>
2024-04-08 00:04:22 +0100

    inputed the double option of saving the variable prior to database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -42,7 +42,7 @@ const session = require("express-session");
 
 const users = [] //create a variable and storing our users in an empty array instead of a database 
 
-app.set("view-engine", "e
+app.set("view-engine", 
 app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET, 
