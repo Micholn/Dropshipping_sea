de551a683a5548bfde3110b38e33533bf3c323d4
Micholn <mdiagbare@gmail.com>
2024-04-07 23:47:54 +0100

    method override

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,7 +44,7 @@ const users = [] //create a variable and storing our users in an empty array ins
 
 app.set("view-engine", "ejs")
 app.use(express.urlencoded({ extended: false })); *
-email and password from our forms 
+email and password from our forms
 app.use(flash());
 app.use(session({
   secret: process.env.SESSION_SECRET, 
