8b5e824b7af28d8f7269db68c63c50f7f035b51b
Micholn <mdiagbare@gmail.com>
2024-04-10 08:28:58 +0100

    initialized the hashing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -33,7 +33,7 @@ let jwtOptions = {
 
 
 
-nst bcrypt = require("bcrypt
+nst bcrypt = require(
 const passport = require("passport") //for authentication 
 const flash = require("express-flash");//to display messages a
 const session = require("express-session"); 
