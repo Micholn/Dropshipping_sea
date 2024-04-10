4952906c499c42ba46a2c18656869c8484ad7c40
Micholn <mdiagbare@gmail.com>
2024-04-10 08:30:08 +0100

    initialized the hashing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,8 +32,6 @@ let jwtOptions = {
 
 
 
-
-nst 
 const passport = require("passport") //for authentication 
 const flash = require("express-flash");//to display messages a
 const session = require("express-session"); 
