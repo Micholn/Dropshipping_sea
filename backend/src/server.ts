a0e8286c2a4d46496d9b7e9f8eeca7e6f2254a70
Micholn <mdiagbare@gmail.com>
2024-04-12 23:16:39 +0100

    joined the public directory through env of server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -57,7 +57,8 @@ class Server {
     this.app.use(cors());
 
     let path = (process.env.NODE_ENV?.toLowerCase() == 'development') ? '/Public' : './Public'
-    var publicDir = require("path").join(__dirname, path)
+    var publicDir = require("path").join(__dirname, path);
+    
   }
 }
 
