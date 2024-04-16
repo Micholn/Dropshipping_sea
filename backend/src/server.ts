17990a93990714c344d4ba7156b8abc553387785
Micholn <mdiagbare@gmail.com>
2024-04-16 23:57:14 +0100

    fixed the confg

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,7 +23,7 @@ let ExtractJwt = passportJWT.ExtractJwt;
 let jwtStrategy = passportJWT.strategy;
 
 let jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderW
+  jwtFromRequest: ExtractJwt.fromAuthHeader
   secretOrKey: "6f733cff-f788-4cf6-95bf-dea989617040",
 }
 
