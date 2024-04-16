2fa251e061c74acab00d42607c1febe7fafcfcac
Micholn <mdiagbare@gmail.com>
2024-04-16 23:59:24 +0100

    fixed the confg

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ import adminRouter from './Routers/adminRouter';
 import publicRouter from './Routers/adminRouter';
 
 
-let ExtractJwt = passportJWT.ExtractJw
+let ExtractJwt = passportJWT.Extract
 let jwtStrategy = passportJWT.strategy;
 
 let jwtOptions = {
